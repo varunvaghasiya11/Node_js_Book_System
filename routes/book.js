abc2aa5a68fd03c27a1ea2d8f27066ea36979ba9
Varun Vaghasiya <varunvaghasiya@Varuns-MacBook-Pro.local>
2025-12-23 21:31:38 +0530

    second commit

--- routes/book.js
+++ routes/book.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bookController = require("../controllers/bookController");
+const bookController = require("../controllers/bookcontroller");
 const upload = require("../middleware/multer");
 
 router.get("/", bookController.getAllBooks);
